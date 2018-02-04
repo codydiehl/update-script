4517468b5f8fc7dd1fa11b88ec4571a1b765eb1a
cody.diehl <diehl17c@gmail.com>
2018-02-04 17:57:45 -0500

    Finalized Brew Section, starting Mac Software

--- update.sh
+++ update.sh
@@ -31,6 +31,7 @@ else
   echo "Not running zsh with prezto"
   echo "Skipping Prezto Update"
 fi 
+sleep 2
 
 # Homebrew update
 if hash brew 2>/dev/null; then
@@ -42,25 +43,43 @@ if hash brew 2>/dev/null; then
   echo "================================"
   brew update
 
-  # update brew software
-  # list brew outdated formula that can be upgraded
-  echo "Checking for outdated brew software that can be updated"
-  brew outdated
+  sleep 2
+  printf '\n'
 
-  # brew upgrade
+  # update brew software
+  echo "================================"
+  echo "Brew Software Update"
+  echo "================================"
   brew upgrade -all
 
+  # list brew outdated formula that can be upgraded
+  #echo "Checking for outdated brew software that can be updated"
+  #brew outdated
 
+  #read -p "Do you want to update the above software? (y/n)" -n 1 -r
+  #if [[ $REPLY =~ ^[Yy]$ ]]; then
+    #echo "You said yes"
+    # brew upgrade
+    #brew upgrade -all
+  #elif [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    #echo "you said something other than yes, skipping"
+  #fi
+  sleep 2
+  printf '\n'
+
+  echo "================================"
+  echo "Cleanup of Brew Junk"
+  echo "================================"
   # CLEANUP JOB
   # list old outdated formula for cleanup
-  brew cleanup -n
+  #brew cleanup -n
   
   # ask if user wants to clean old outdated formula as brew leaves by default
   read -p "Do you want to clean up old outdated brew formulae? (y/n)" -n 1 -r
   echo # (optional) move to a new line
   if [[ $REPLY =~ ^[Yy]$ ]]; then
 
-    echo "You said yes"
+    echo "Cleaning up old outdated packages"
     brew cleanup
 
   elif [[ ! $REPLY =~ ^[Yy]$ ]]; then
@@ -71,3 +90,5 @@ if hash brew 2>/dev/null; then
     # handle exits from shell or function but dont exit interactive shell
   fi
 fi
+
+
