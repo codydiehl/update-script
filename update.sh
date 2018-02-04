d26f53efea8f6828418643e66b4db495b9352909
cody.diehl <diehl17c@gmail.com>
2018-02-04 16:35:22 -0500

    Added homebrew updates

--- update.sh
+++ update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # get platform 
 if [[ "$OSTYPE" == 'linux' ]]; then
   platform='Linux'
@@ -16,13 +15,59 @@ else
   platform='Unknown'
 fi
 
-echo $platform
+echo "Platform is $platform"
 
 
 # MacOS specific
 
 # zprezto update
 if [[ "$SHELL" == *'zsh' && -e "$HOME/.zpreztorc" ]]; then
-  echo "Running ZSH and prezto"
+  echo "================================"
+  echo "Running prezto update"
+  echo "================================"
+  `which zsh` -i -c zprezto-update  # ~/.zprezto/init.zsh zprezto-update function
+
+else 
+  echo "Not running zsh with prezto"
+  echo "Skipping Prezto Update"
 fi 
 
+# Homebrew update
+if hash brew 2>/dev/null; then
+  echo "Brew Exists"
+  
+  # brew update formulae and homebrew itself
+  echo "================================"
+  echo "Update homebrew itself"
+  echo "================================"
+  brew update
+
+  # update brew software
+  # list brew outdated formula that can be upgraded
+  echo "Checking for outdated brew software that can be updated"
+  brew outdated
+
+  # brew upgrade
+  brew upgrade -all
+
+
+  # CLEANUP JOB
+  # list old outdated formula for cleanup
+  brew cleanup -n
+  
+  # ask if user wants to clean old outdated formula as brew leaves by default
+  read -p "Do you want to clean up old outdated brew formulae? (y/n)" -n 1 -r
+  echo # (optional) move to a new line
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+
+    echo "You said yes"
+    brew cleanup
+
+  elif [[ ! $REPLY =~ ^[Yy]$ ]]; then
+
+    echo "Skipping brew cleanup of outdated formulae"
+
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 
+    # handle exits from shell or function but dont exit interactive shell
+  fi
+fi
