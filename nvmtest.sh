bfa5b1ee2ecdd486d9231e5484c62e1f7ba8bb80
cody.diehl <diehl17c@gmail.com>
2018-02-05 01:22:09 -0500

    complete creation of nvmtest, need testing

--- nvmtest.sh
+++ nvmtest.sh
@@ -6,15 +6,17 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then # start reply checks
 	printf '\n'
 	echo "Using nvm to manage node.js"
 	printf '\n'
+	sleep 2
 
 	echo "-Checking if nvm is already installed-"
-	if type nvm &>/dev/null; then # start nvm check
+	if [ ${NVM_DIR+x} ]; then # start nvm check
 		echo "nvm is already installed..."
 		
 	else
 		echo "nvm is not installed..."
     # macos specific install
 		if [[ $OSTYPE =~ "darwin" ]]; then # start OS checks 
+			printf '\n'
 			brew install nvm
 
     # linux specific install
@@ -22,46 +24,50 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then # start reply checks
       mkdir $HOME/.nvm
       git clone https://github.com/creationix/nvm.git $HOME/.nvm
       (cd $HOME/.nvm && git checkout $(git describe --tags `git rev-list --tags --max-count=1`))
+			# Initialize nvm
+			if [[ "$SHELL" =~ "zsh" ]]; then # start shell checks
+				echo 'export NVM_DIR="$HOME/.nvm"' >> $HOME/.zshrc
+				echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $HOME/.zshrc
+				source $HOME/.nvm/nvm.sh
+
+			elif [[ "$SHELL" =~ "bash" ]]; then # cont. shell checks
+				echo 'export NVM_DIR="$HOME/.nvm"' >> $HOME/.bashrc
+				echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $HOME/.bashrc
+				source $HOME/.nvm/nvm.sh
+
+			fi # end shell checks
     fi # end os checks
-		
-		# Initialize nvm
-		# this check applies to all OS installs
-		if [[ "$SHELL" =~ "zsh" ]]; then # start shell checks
-			echo 'export NVM_DIR="$HOME/.nvm"' >> $HOME/.zshrc
-			echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $HOME/.zshrc
-			source $HOME/.nvm/nvm.sh
-
-		elif [[ "$SHELL" =~ "bash" ]]; then # cont. shell checks
-			echo 'export NVM_DIR="$HOME/.nvm"' >> $HOME/.bashrc
-			echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $HOME/.bashrc
-			source $HOME/.nvm/nvm.sh
-
-		fi # end shell checks
-
-		printf '\n'
-    echo "-Install latest node.js via nvm-"
-    printf '\n'
-    if [[ `node -v` == `nvm ls node | head -1 | awk '{print $2}'` ]]; then # nvm compare
-      echo "Already on the latest version of node.js:" `node -v`
-    else 
-      echo "New version of node.js is available..."
-      printf '\n'
-      echo "Current version of node.js:" `node -v`
-      echo "Latest version of node.js:" `nvm ls node | head -1 | awk '{print $2}'`
-			printf '\n'
-
-      read -p "Do you want to update node.js to `nvm ls node | head -1 | awk '{print $2}'`? (y/n)" -n 1 -r
-  		if [[ $REPLY =~ ^[Yy]$ ]]; then # start nvm update check
-				printf '\n'
-    		echo "Updating node.js..."
-					nvm install node
-  		elif [[ ! $REPLY =~ ^[Yy]$ ]]; then # cont. nvm update check
-    		echo "Skipping node.js update"
-  		fi # end reply check for node update
-  	fi # end nvm compare check
 	fi # end nvm install check
+sleep 2
 
-elif [[ ! $REPLY =~ ^[Yy]$ ]]; then # cont reply checks
+	# now that NVM is installed
+	# Using NVM to update
+	printf '\n'
+	echo "-Install latest node.js via nvm-"
+	printf '\n'
+	if [[ `node -v` == `$NVM_DIR/nvm.sh ls node | head -1 | awk '{print $2}'` ]]; then # nvm compare
+		echo "Already on the latest version of node.js:" `node -v`
+	else 
+		echo "New version of node.js is available..."
+		printf '\n'
+		echo "Current version of node.js:" `node -v`
+		echo "Latest version of node.js:" `$NVM_DIR/nvm.sh ls node | head -1 | awk '{print $2}'`
+		printf '\n'
+
+		read -p "Do you want to update node.js to `$NVM_DIR/nvm.sh ls node | head -1 | awk '{print $2}'`? (y/n)" -n 1 -r
+		if [[ $REPLY =~ ^[Yy]$ ]]; then # start nvm update check
+			printf '\n'
+			echo "Updating node.js..."
+				nvm install node
+		elif [[ ! $REPLY =~ ^[Yy]$ ]]; then # cont. nvm update check
+			printf '\n'
+			echo "Skipping node.js update"
+		fi # end reply check for node update
+	fi # end nvm compare check
+
+
+
+elif [[ ! $REPLY =~ ^[Yy]$ ]]; then # cont. reply checks
 	echo "You chose not to use nvm, lets use n"
 
 	ncheck=`npm view n | head -2`
